d53c43bb69074747449bb84ecd382f9182955f31
Rizky Fauzi <rizky.fauzi@code.id>
2025-02-20 02:37:42 +0700

    Initial Commit

---
+++ MiniAppTester/MiniAppTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniAppTesterApp.swift
+//  MiniAppTester
+//
+//  Created by Rizky Fauzi on 20/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniAppTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
