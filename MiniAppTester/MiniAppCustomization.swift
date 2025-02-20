4a2005bdee7ac1e32dc74cc3f15cdb193a1e5a2d
Rizky Fauzi <rizky.fauzi@code.id>
2025-02-20 14:26:42 +0700

    first commit

---
+++ MiniAppTester/MiniAppCustomization.swift
@@ -0,0 +1,8 @@
+//
+//  MiniAppCustomization.swift
+//  MiniAppTester
+//
+//  Created by Rizky Fauzi on 20/02/25.
+//
+
+import Foundation
---
+++ MiniAppTester/UIViewControllerFinder.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewControllerFinder.swift
+//  MiniAppTester
+//
+//  Created by Rizky Fauzi on 20/02/25.
+//
+
+import Foundation
